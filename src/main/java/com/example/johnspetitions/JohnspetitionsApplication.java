3649558422635585e10ffb0e9bcab4429f4063fe
John Maguire <j_mag_uidhir@outlook.ie>
2025-11-29 21:32:51 +0000

    Jenkinsfile change to deploy app to tomcat container #8
    amended main

--- src/main/java/com/example/johnspetitions/JohnspetitionsApplication.java
+++ src/main/java/com/example/johnspetitions/JohnspetitionsApplication.java
@@ -2,9 +2,15 @@ package com.example.johnspetitions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JohnspetitionsApplication {
+public class JohnspetitionsApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JohnspetitionsApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JohnspetitionsApplication.class, args);
