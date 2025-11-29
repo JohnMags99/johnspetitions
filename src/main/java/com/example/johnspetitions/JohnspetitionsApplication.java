5863e205787c179b1b3609f589bb6271c909556c
John Maguire <j_mag_uidhir@outlook.ie>
2025-11-29 15:13:56 +0000

    added Jenkinsfile and misc changes

--- src/main/java/com/example/johnspetitions/JohnspetitionsApplication.java
+++ src/main/java/com/example/johnspetitions/JohnspetitionsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JohnspetitionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JohnspetitionsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JohnspetitionsApplication.class, args);
+    }
 
 }
