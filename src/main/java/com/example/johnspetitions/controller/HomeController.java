3410bb6041a7b72b09fb0856af58a4f00650baf8
John Maguire <j_mag_uidhir@outlook.ie>
2025-11-29 23:29:17 +0000

    added first template
    added thymeleafe dependency
    
    added bootstrap framework for css

--- src/main/java/com/example/johnspetitions/controller/HomeController.java
+++ src/main/java/com/example/johnspetitions/controller/HomeController.java
@@ -1,14 +1,14 @@
 package com.example.johnspetitions.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from johnspetitions!";
+        return "home";
     }
 }
 
